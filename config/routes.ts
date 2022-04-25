37fa795ae277c37e68eece65cddc0c501ead935f
jcc <ccj6161@163.com>
2022-04-25 19:53:48 +0800

    上传工程

---
+++ config/routes.ts
@@ -0,0 +1,66 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+  // new
+  { 
+    path: '/new', 
+    name: 'new', 
+    icon: 'user', 
+    routes: [ 
+        { 
+            path: '/new/analysis', 
+            name: 'analysis', 
+            component: './New/NewAnalysis', 
+        },
+    ], 
+  },
+];
---
+++ src/pages/New/NewAnalysis.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+ 
+class NewAnalysis extends React.Component {
+    render() {
+      return ( <div>第一个中后台项目</div> );
+    }
+}
+export default NewAnalysis;
